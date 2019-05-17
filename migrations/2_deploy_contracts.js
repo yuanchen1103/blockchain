bd1d92c3b8a94ab89b5615d1308e7607fb22c01e
Evis Cheng <e920528@gmail.com>
2019-05-17 23:25:21 +0800

    [Fix] migrate

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-var Arts = artifacts.require("./Arts.sol")
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
-  deployer.deploy(Arts)
 };
